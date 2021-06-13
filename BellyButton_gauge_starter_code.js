eb7a524b248a5a0cc051fad3d90091123ded2590
Arelys Rosado <80235884+arelysrsd87@users.noreply.github.com>
2021-06-13 16:28:34 -0500

    26th commit

--- BellyButton_gauge_starter_code.js
+++ BellyButton_gauge_starter_code.js
@@ -29,31 +29,41 @@ function buildCharts(sample) {
    
     
     // 4. Create the trace for the gauge chart.
-    var gaugeData = [
+    var gaugeData = [{
+      domain: { x: [0, 1], y: [0, 1] },
+      value: wfreq,
+      type: "indicator",
+      mode: "gauge+number",
+      gauge: {
+          axis: {
+              range: [0, 9],
+              tickmode: 'linear',
+              tickfont: {
+                  size: 15
+              }
+          },
+          bar: { color: 'rgba(8,29,88,0)' },
+          steps: [
+              { range: [0, 1], color: 'rgb(255,255,217)' },
+              { range: [1, 2], color: 'rgb(237,248,217)' },
+              { range: [2, 3], color: 'rgb(199,233,180)' },
+              { range: [3, 4], color: 'rgb(127,205,187)' },
+              { range: [4, 5], color: 'rgb(65,182,196)' },
+              { range: [5, 6], color: 'rgb(29,145,192)' },
+              { range: [6, 7], color: 'rgb(34,94,168)' },
+              { range: [7, 8], color: 'rgb(37,52,148)' },
+              { range: [8, 9], color: 'rgb(8,29,88)' }
+          ]
+      }
+  };
      
     ];
     
     // 5. Create the layout  the gauge chart.
     var gaugeLayout = { 
-     value: wfreq,
-     type: "indicator",
-     mode: "gauge+number",
      title: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week",
      maximum range: 10,
-     bar: {color: 'rgba(8,29,88,0)'}, 
-     steps: [
-      { range: [0, 1], color: 'rgb(255,255,217)' },
-      { range: [1, 2], color: 'rgb(237,248,217)' },
-      { range: [2, 3], color: 'rgb(199,233,180)' },
-      { range: [3, 4], color: 'rgb(127,205,187)' },
-      { range: [4, 5], color: 'rgb(65,182,196)' },
-      { range: [5, 6], color: 'rgb(29,145,192)' },
-      { range: [6, 7], color: 'rgb(34,94,168)' },
-      { range: [7, 8], color: 'rgb(37,52,148)' },
-      { range: [8, 9], color: 'rgb(8,29,88)' }
-  ]
-
-    };
+    
 
     // 6. Use Plotly to plot the gauge data and layout.
     Plotly.newPlot("gauge", gaugeData, gaugeLayout);
